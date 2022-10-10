75219b2ebdfb11d5f5af222b4a34632185b3e2ac
Enrique <kiketos1999@gmail.com>
2022-10-10 21:48:32 +0200

    Feature: Push Notifications

--- app/src/main/java/com/youngsdeveloper/bus_murcia/activites/MainActivity.kt
+++ app/src/main/java/com/youngsdeveloper/bus_murcia/activites/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.youngsdeveloper.bus_murcia.activites
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -37,6 +44,55 @@ class MainActivity : AppCompatActivity() {
 
         val bottom_navigation = bottomNavigationView
         bottom_navigation.setupWithNavController(navController)
+
+
+        askNotificationPermission()
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+
+
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(getString(R.string.notifications_request))
+                    .setPositiveButton("De acuerdo",
+                        DialogInterface.OnClickListener { dialog, id ->
+                            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                        })
+                    .setNegativeButton("No, gracias.",
+                        DialogInterface.OnClickListener { dialog, id ->
+                            // User cancelled the dialog
+                        })
+                // Create the AlertDialog object and return it
+                builder.create().show()
+
+
+                // DONE: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/youngsdeveloper/bus_murcia/services/MessagingService.java
@@ -0,0 +1,6 @@
+package com.youngsdeveloper.bus_murcia.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MessagingService extends FirebaseMessagingService {
+}
